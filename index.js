99902e0c4767c0b88a2f23d352d388e6c97ab087
William Sulinski <w@williamsulinski.com>
2020-01-16 12:56:30 -0800

    Changed port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./server.js');
 const port = process.env.PORT || 9000;
 
 server
-    .listen(9000, () =>
+    .listen(port, () =>
         console.log('\n*** Server Running on http://localhost:9000 *** \n')
     );
