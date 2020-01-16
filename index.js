1da260a17770d29f52945dd37e4978c6ad2accf2
William Sulinski <w@williamsulinski.com>
2020-01-16 10:46:31 -0800

    Add port config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server.js');
 
+const port = process.env.PORT || 9000;
+
 server
     .listen(9000, () =>
         console.log('\n*** Server Running on http://localhost:9000 *** \n')
